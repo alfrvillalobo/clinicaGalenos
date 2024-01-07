db461c602f5e2ffe6eb04745ae4c6105405000cf
alfrvillalobo <alfr.villalobo@duocuc.cl>
2024-01-06 21:55:32 -0300

    update

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+import { MainAdminPage } from '../main-admin/main-admin.page';
+import { MainPacientePage } from '../main-paciente/main-paciente.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'Menu',
+        loadChildren: () => import('../../pages/main-paciente/main-paciente.module').then(m => m.MainPacientePageModule),
+      },
+      {
+        path: 'Toma de Hora',
+        loadChildren: () => import('../../pages/toma-hora/toma-hora.module').then(m => m.TomaHoraPageModule),
+      },
+      {
+        path: 'Mi Agenda',
+        loadChildren: () => import('../../pages/mi-agenda/mi-agenda.module').then(m => m.MiAgendaPageModule),
+      },
+      {
+        path: 'Perfil',
+        loadChildren: () => import('../../pages/perfil/perfil.module').then(m => m.PerfilPageModule),
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
