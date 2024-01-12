ff715fb7e54d23457b6b19fec430df4aa1ca4301
alfrvillalobo <alfr.villalobo@duocuc.cl>
2024-01-12 14:49:09 -0300

    update

--- src/app/pages/restablecer-password/restablecer-password.page.ts
+++ src/app/pages/restablecer-password/restablecer-password.page.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-restablecer-password',
-  templateUrl: './restablecer-password.page.html',
-  styleUrls: ['./restablecer-password.page.scss'],
+  templateUrl: 'restablecer-password.page.html',
+  styleUrls: ['restablecer-password.page.scss'],
 })
-export class RestablecerPasswordPage implements OnInit {
+export class RestablecerPasswordPage {
 
-  constructor() { }
+  correoForm: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    private alertController: AlertController,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    // Simplificamos temporalmente la validación eliminando Validators.email
+    this.correoForm = this.formBuilder.group({
+      correoElectronico: ['', [Validators.required]]
+    });
   }
 
+  async enviarCorreo() {
+    console.log('Formulario válido:', this.correoForm.valid);
+
+    if (this.correoForm.valid) {
+      console.log('Enviando correo...');
+
+      // Aquí iría la lógica para enviar el correo electrónico
+
+      const alert = await this.alertController.create({
+        header: 'Correo Enviado',
+        message: 'Se ha enviado un mensaje a su correo electrónico.',
+        buttons: [
+          {
+            text: 'OK',
+            handler: () => {
+              // Redirección a la página de home
+              this.router.navigate(['/home']);
+            }
+          }
+        ]
+      });
+
+      await alert.present();
+    } else {
+      console.log('Formulario inválido. Detalles:', this.correoForm.errors);
+
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Ingrese un correo electrónico válido. Asegúrese de seguir el formato correcto.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    }
+  }
 }
