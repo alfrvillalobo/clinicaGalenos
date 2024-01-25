72b8328220d8f387ece21993608167413a4cd22d
alfrvillalobo <alfr.villalobo@duocuc.cl>
2024-01-25 13:56:22 -0300

    se actualizo el perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
@@ -9,46 +9,26 @@ import { HelperService } from 'src/app/services/helper.service';
   templateUrl: 'perfil.page.html',
   styleUrls: ['perfil.page.scss'],
 })
-export class PerfilPage {
+export class PerfilPage implements OnInit {
   nombre: string = 'Nombre del Usuario';
   rut: string = '123456789';
   prevision: string = 'Isapre';
   email: string = 'usuario@example.com';
   numero: string = '123456789';
+  userData: any;
 
-  constructor(private router: Router, 
-              private alertController: AlertController,
+  constructor(private router: Router,            
               private auth: AuthService,
               private helper: HelperService) {}
-/**
-  async confirmarCerrarSesion() {
-    const alert = await this.alertController.create({
-      header: 'Cerrar Sesión',
-      message: '¿Desea cerrar sesión?',
-      buttons: [
-        {
-          text: 'No',
-          role: 'cancel',
-          handler: () => {           
-          },
-        },
-        {
-          text: 'Sí',
-          handler: () => {
-            this.cerrarSesion();
-          },
-        },
-      ],
+  ngOnInit() {
+    this.auth.getCurrentUser().subscribe(async (user) => {
+      if (user) {
+        // Aquí puedes hacer algo con la información del usuario
+        this.userData = await this.auth.getUserAdditionalInfo(user.uid);
+      }
     });
-
-    await alert.present();
   }
 
-  cerrarSesion() {    
-    this.router.navigateByUrl('/home');
-  }
- */
-  
   logout() {
     this.auth.Logut();
     this.helper.presentToast('Cierre de sesión exitoso');
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { user } from '../models/usuario';
+import { StorageService } from './storage.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private angularfire: AngularFireAuth) { }
+  constructor(private angularfire: AngularFireAuth,
+              private afAuth: AngularFireAuth,
+              private firestore: AngularFirestore) { }
 
   login(correo:string, password: string) {
     return this.angularfire.signInWithEmailAndPassword(correo,password);    
@@ -18,5 +22,19 @@ export class AuthService {
 
   registrarUser(datos: user) {
     return this.angularfire.createUserWithEmailAndPassword(datos.correo, datos.password);
+  }
+
+  getCurrentUser() {
+    return this.afAuth.authState;
+  }
+
+  async getUserAdditionalInfo(uid: string) {
+    const userDoc = await this.firestore.collection('Usuarios').doc(uid).ref.get();
+    if (userDoc.exists) {
+      return userDoc.data();
+    } else {
+      console.log('Usuario no encontrado en Firestore');
+      return null;
+    }
   }
 }
